aa37ee84b9ab4fb4ce4c1fa7a613a72713a040e5
MarcosF19 <86076983+MarcLipeSL20@users.noreply.github.com>
2021-12-02 00:48:19 -0300

    Correr

--- new/script.js
+++ new/script.js
@@ -1,5 +1,12 @@
 function carregar() {
-    var msg = window.document.getElementById('msg'); 
-    
-    var 
+    var dat = new Date(); 
+    var ano = data.getFullYear(); 
+    var fano = document.getElementById('txtano'); 
+    var res = document.querySelector('div#res'); 
+
+    if(fano.value) {
+
+    } else if() {
+
+    }
 }
