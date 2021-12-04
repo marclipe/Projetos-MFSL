bf0adc386062a7cddfd4a1e5d169f7695a8688a7
MarcosF19 <86076983+MarcLipeSL20@users.noreply.github.com>
2021-12-03 23:57:03 -0300

    Look me

--- new/script.js
+++ new/script.js
@@ -1,16 +1,14 @@
 function carregar() {
-    var tempo = new Date();
-    var ano = data.getTime; 
-
-    if () {
+    var data = new Date();
+    var min = data.getMinutes(); 
+    var fhora = document.getElementById('txttempo');
+    var res = document.querySelector('div#res'); 
+    
+    if (fhora.value.length == 0 || Number(fhora.value) > min) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-
-        if () {
-
-        } else if () {
-
-        }
+        window.alert('Vamos lá meu anjo!!'); 
     }
-
+    
+    
 }
