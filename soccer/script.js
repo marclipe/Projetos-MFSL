1543f0a754b3549d76d30dad0adab4258df58d65
MarcosF19 <86076983+MarcLipeSL20@users.noreply.github.com>
2021-12-07 22:19:50 -0300

    Ok

--- soccer/script.js
+++ soccer/script.js
@@ -7,22 +7,18 @@ function carregar() {
     if (fhora.value.length == 0 || Number(fhora.value) > min) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var radtime = document.getElementById('radtime');
+        var radtime = document.getElementsById('radtime');
         var tipo_do_tempo = ''; 
         var tempo = Number(fhora.value); 
-        var img = document.createElement('img'); 
-        img.setAttribute('id', 'foto'); 
         if (radtime[0].checked) {
-            tipo_do_tempo = "Primeiro"; 
-            window.document("Estamos no Primeiro Tempo"); 
-            if(tempo >= 0 && tempo > 45){
-                window.alert("Primeiro tempo de jogo")
+            tipo_do_tempo = 'Primeiro'; 
+            if(tempo >= 0 && tempo >= 45){
+                window.alert("Primeiro tempo de jogo"); 
             } else if (tempo <= 91 && tempo >= 105) {
-                window.alert("Primeiro tempo de Prorrogação!")
+                window.alert("Primeiro tempo de Prorrogação!"); 
             }
         } else if(radtime[1].checked) {
-            tipo_do_tempo = "Segundo"; 
-            window.document("Estamos no Segundo Tempo"); 
+            tipo_do_tempo = 'Segundo';  
             if (tempo <= 46 && tempo >= 90) {
                 window.alert("Estamos no Segundo tempo de jogo")
             } else if (tempo <= 106 && tempo >= 120) {
@@ -31,9 +27,6 @@ function carregar() {
         } else {
             window.alert("Aí acabou o jogo eu tapado");
         }
-
-    }
-    
+    }   
     res.innerHTML = `Você está no tempo ${tipo_do_tempo} e ${tempo}`
-    res.appendChild(img);    
 }
